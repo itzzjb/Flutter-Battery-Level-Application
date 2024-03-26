511b6ecc058a39d107b1c73e497ec332de2d98b7
Januda Bethmin <desilvabethmin@gmail.com>
2024-03-26 22:35:41 +0530

    last commit

--- android/app/src/main/kotlin/com/example/maridian/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/maridian/MainActivity.kt
@@ -1,5 +1,68 @@
 package com.example.maridian
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import com.google.firebase.database.annotations.NotNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+
+  // We need to listen from the same method channel here.
+  private val CHANNEL = "com.januda.flutter/battery"
+
+  // We need to configure the channel we are calling
+  override fun configureFlutterEngine(@NotNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    MethodChannel(
+      flutterEngine.dartExecutor.binaryMessenger,
+      CHANNEL
+    ).setMethodCallHandler { call, result ->
+      // This method is invoked on the main thread.
+      // Doing the mapping here
+      if (call.method == "getBatteryLevel") {
+        val batteryLevel = getBatteryLevel()
+
+        if (batteryLevel != -1) {
+          result.success(batteryLevel)
+        } else {
+          result.error("UNAVAILABLE", "Battery level not available.", null)
+        }
+      } else {
+        result.notImplemented()
+      }
+
+    }
+
+  }
+
+  // This is the implementation of the function getBatteryLevel() for android in Kotlin
+  // Note that the name of the function is same as the function name defined in the flutter file.
+  // This is a good practice so we can avoid confusion
+  // We can use another name too. Because we are doing the mapping above.
+  private fun getBatteryLevel(): Int {
+    val batteryLevel: Int
+    if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+      val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+    } else {
+      val intent = ContextWrapper(applicationContext).registerReceiver(
+        null,
+        IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+      )
+      batteryLevel =
+        intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(
+          BatteryManager.EXTRA_SCALE,
+          -1
+        )
+    }
+
+    return batteryLevel
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -8,11 +9,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   int _counter = 0;
+  // This is a example for a syncronous method.
+  // Here within the method we don't rely on anthing from the outside world / anything that is blocking our execution
+  // ex - We don't expect a huge cpu overload, We don't expect for network calls, We don't expect a I/O bpund operation to happen
+  // Simply these tasks won't take that much time becuase of outside reasons so they are called as synchronous tasks.
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
+  }
+
+  // We need to create a method channel instance in the flutter side.
+  // We need to have a unique identifier for a method channel
+  // So we have a id like 'com.januda.flutter/battery'
+  // We can communicate between flutter and the specifi OS using this.
+  // Either we can send information from flutter to OS or OS to flutter 
+  // Here we are expecting some data to get returned from the OS
+  // Note that we can call multiple methods from a single channel.
+  static const platform = MethodChannel('com.januda.flutter/battery');
+
+  // We need to get he value for the battery level
+  // Instatiting a varibale for it and give a default value
+  // We will call the method channel and call the platform and get the battery level and assign it
+  String _batteryLevel = "Unknown Battery Level";
+
+  // Any task that will time to execute because of a outside reason is called as asyncronous tasks.
+  // ex - We expect a huge cpu overload, We expect for network calls, We expect a I/O bpund operation to happen
+  // We can't gurantee how much time it will take to execute this method
+  // async -> Says this task gonna take little bit longer, so don't wait for the return value.
+  // Futute -> Instead of waiting , somewhere in the future the value will be returned/recieved.
+  // Method will be executed in the background.
+  Future<String> _getBatteryLevel() async {
+
+    String batteryLevel;
+    // **** Method channel call goes here **** 
+    // We use try catch for error handling
+    try {
+      // We are invoking a method and give that a name. 
+      // We can give it a any name.
+      // But the method name that should be in the underlying OS flies should be same as this.
+      // We need to undeline this method in the underlying OS.
+      // <int> -> Assuring the return type of the methos as int
+      // await -> Because this is a asyncronous method and we don't know how much time it will take.
+      final result = await platform.invokeMethod<int>('getBatteryLevel');
+      batteryLevel = 'Battery level is at $result %';
+      // There might be many errors. So we are gonna catch those PlatformExceptions
+    } on PlatformException {
+      // We are showing the error message on top of the flutter engine.
+      batteryLevel = 'Failed to get battery level';
+    }
+
+    // We need setState() method inorder the update the ui when the state changes
+    // state change -> When we get the battery value from the platform
+    setState(() {
+      // Updates the value of the _battery Level according the value we got from the platform
+      _batteryLevel = batteryLevel;
+    });
+
+    // todo: added to resolve the error
+    return '';
   }
 
   @override
@@ -26,21 +83,21 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            // Showing the battery level in the UI
             Text(
-              '$_counter',
+              _batteryLevel,
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        // Make the button call getBatteryLevel() Function
+        onPressed: _getBatteryLevel,
+        tooltip: 'Battery Level',
+        child: const Icon(Icons.battery_unknown),
       ),
     );
   }
+
 }
